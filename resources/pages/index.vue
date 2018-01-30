424d25f01945a782b85b68de968fa89d67c11531
Старостенко Максим Станиславович <m.starostenko@e2e4online.ru>
2018-01-30 23:10:32 +0700

    ++

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -1,19 +1,57 @@
-<template>
-  <section class="container">
-    <img src="/img/logo.png" alt="Adonuxt logo"/>
-    <h1>Universal Vue.js Applications Framework</h1>
-    <nuxt-link :to="{ name: 'about' }">About</nuxt-link>
-  </section>
+<template lang="pug">
+  v-layout(
+   column
+   justify-center
+   align-center
+  )
+    v-flex(
+     xs12
+     sm8
+     md6
+    )
+      .text-xs-center
+        img(
+         src="/v.png"
+         alt="Vuetify.js"
+         class="mb-5"
+        )
+
+      v-card
+        v-card-title.headline
+          | Welcome to the Vuetify + Nuxt.js template
+
+        v-card-text
+          p Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          p For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>
+          p If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">discord</a>.</p>
+          p Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">issue board</a>.</p>
+          p Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+
+          .text-xs-right
+            | <em><small>&mdash; John Leider</small></em>
+
+          hr.my-3
+          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
+          <br>
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
+
+        v-card-actions
+          v-spacer
+          v-btn(
+            color="primary"
+            flat
+            nuxt
+            to="/inspire"
+          ) Continue
+
 </template>
 
 <script>
-  export default {
-    name: 'page-index'
-  }
+
+export default {
+};
+
 </script>
 
-<style scoped>
-  h1 {
-    margin: 50px 0;
-  }
+<style lang="scss">
 </style>
