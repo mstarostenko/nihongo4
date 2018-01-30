1012751829dbdf0149bdb847218d64915f12a152
Старостенко Максим Станиславович <m.starostenko@e2e4online.ru>
2018-01-31 00:31:07 +0700

    Восстановил default лайаут

--- resources/layouts/default.vue
+++ resources/layouts/default.vue
@@ -1,50 +1,171 @@
-<template>
-  <div>
-    <nuxt/>
-    <my-footer/>
-  </div>
+<template lang="pug">
+    v-app.b-app(
+        light
+    )
+        v-navigation-drawer.b-nav(
+            dark
+            persistent
+            :mini-variant="miniVariant"
+            :clipped="clipped"
+            v-model="drawer"
+            app
+        )
+
+            v-list.b-nav-list(
+                dark
+            )
+                v-list-group.b-nav-list__group(
+                    v-for="item in items"
+                    :value="item.active"
+                    v-bind:key="item.title"
+                )
+                    v-list-tile.b-nav-list__item(
+                        slot="item"
+                        @click=""
+                    )
+                        v-list-tile-action
+                            v-icon
+                                | {{ item.action }}
+
+                        v-list-tile-content
+                            v-list-tile-title.title
+                                | {{ item.title }}
+
+                        v-list-tile-action
+                            v-icon
+                                | keyboard_arrow_down
+
+                    v-list-tile.b-nav-list__subitem(
+                        router
+                        v-for="subItem in item.items"
+                        :key="subItem.title"
+                        active-class="b__active"
+                        :to="subItem.to"
+                        exact
+                    )
+                        v-list-tile-content.b-nav-list__subitem-content
+                            v-list-tile-title.b-nav-list__subitem-title
+                                | {{ subItem.title }}
+
+                        v-list-tile-action
+                            v-icon.white--text
+                                | {{ subItem.action }}
+
+        v-toolbar(
+            dark
+            fixed
+            app
+            :clipped-left="clipped"
+        )
+
+            v-toolbar-side-icon(
+                dark
+                @click="drawer = !drawer"
+            )
+
+            v-btn(
+                dark
+                icon
+                @click.stop="miniVariant = !miniVariant"
+            )
+                v-icon(
+                    dark
+                    v-html="miniVariant ? 'chevron_right' : 'chevron_left'"
+                )
+
+            v-btn(
+                dark
+                icon
+                @click.stop="clipped = !clipped"
+            )
+                v-icon
+                    | web
+
+            v-btn(
+                dark
+                icon
+                @click.stop="fixed = !fixed"
+            )
+                v-icon(
+                    dark
+                )
+                    | remove
+
+            v-toolbar-title(
+                dark
+                v-text="title"
+            )
+
+
+        main
+            v-content
+                v-container
+                    nuxt
+
+
+        v-footer(
+            :fixed="fixed"
+            app
+        )
+        span &copy; 2017
+
 </template>
 
 <script>
-  const MyFooter = () => import('~/components/MyFooter')
+export default {
+    data () {
+        return {
+            clipped: false,
+            drawer: true,
+            fixed: false,
+            items: [
+                {
+                    action: 'store_mall_directory',
+                    title: '宿題',
+                    items: [
+                        {
+                            title: '11月 5日',
+                            to: '/patterns',
+                        },
+                        {
+                            title: '11月 5日',
+                            to: '/',
+                        },
+                    ],
+                },
 
-  export default {
-    name: 'layout-default',
-
-    components: {
-      MyFooter
-    }
-  }
+                {
+                    action: 'school',
+                    title: 'Education',
+                    active: false,
+                    items: [
+                        {
+                            title: 'Patterns',
+                            action: 'aspect_ratio',
+                            to: 'patterns',
+                        },
+                    ],
+                },
+            ],
+            miniVariant: false,
+            right: true,
+            // rightDrawer: false,
+            title: 'Vuetify.js',
+        };
+    },
+};
 </script>
 
-<style>
-  .container {
-    margin: 0;
-    width: 100%;
-    padding: 100px 0;
-    text-align: center;
-  }
 
-  .button, .button:visited {
-    display: inline-block;
-    color: #744d82;
-    letter-spacing: 1px;
-    background-color: #fff;
-    border: 2px solid #744d82;
-    text-decoration: none;
-    text-transform: uppercase;
-    padding: 15px 45px;
-  }
+<style lang="scss">
 
-  .button:hover, .button:focus {
-    color: #fff;
-    background-color: #744d82;
-  }
+// .b-nav-list__subitem {
 
-  .title {
-    color: #505153;
-    font-weight: 300;
-    font-size: 2.5em;
-    margin: 0;
-  }
+//   .list__tile--link.b__active {
+//     .b-nav-list__subitem-title {
+//       background-color: green !important;
+//     }
+//   }
+// }
+
 </style>
