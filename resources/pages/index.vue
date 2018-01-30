1fa605cef31700bb35b9d34b839e96acf32fe325
Старостенко Максим Станиславович <m.starostenko@e2e4online.ru>
2018-01-30 22:34:08 +0700

    Initial commit

---
+++ resources/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <img src="/img/logo.png" alt="Adonuxt logo"/>
+    <h1>Universal Vue.js Applications Framework</h1>
+    <nuxt-link :to="{ name: 'about' }">About</nuxt-link>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'page-index'
+  }
+</script>
+
+<style scoped>
+  h1 {
+    margin: 50px 0;
+  }
+</style>
